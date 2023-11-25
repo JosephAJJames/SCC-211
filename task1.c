a57515fde4995addec94c3407648cf0f08b211cb
Joseph James <Joe.Centurion.5@gmail.com>
2023-11-25 20:03:01 +0000

    BytesPerSector

--- task1.c
+++ task1.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+// #include <unistd.h> //for unix systems
+#include <io.h> //for windows systems
 
 #define numOfBytesToRead 8
 int main()
@@ -11,22 +12,26 @@ int main()
     int byteOffset = 5;
 
     int file = open("text.txt", O_RDONLY | O_CREAT);
-    
-    if (file != -1) {
+
+    if (file != -1)
+    {
         off_t offset = lseek(file, byteOffset, SEEK_CUR);
 
-        if (offset != -1) {
+        if (offset != -1)
+        {
             char buffer[numOfBytesToRead];
             size_t readFile = read(file, buffer, numOfBytesToRead);
-            printf("%s", buffer, numOfBytesToRead);
-
-        } else {
-            printf("Theres's been an error moving the file pointer.");
-            return -1;
+            printf("%s", buffer);
         }
-
-    } else {
+        else
+        {
+            printf("Theres's been an error moving the file pointer.");
+            return 1;
+        }
+    }
+    else
+    {
         printf("There's been an error opening the files");
-        return -1;
+        return 1;
     }
 }
--- task2.c
+++ task2.c
@@ -3,38 +3,49 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+// #include <unistd.h> //for unix systems
+#include <io.h> //for windows systems
+#include <stdint.h>
 
+/*http://www.maverick-os.dk/FileSystemFormats/FAT16_FileSystem.html#:~:text=The%20FAT16%20file%20system%20uses,volume%20(volume%20%3D%20partition).*/
 
-char * searchFile(int bytpeOffset, int numOofBytesToRead, char path[], char * buffer)
+unsigned char *searchFile(int byteOffset, int numOfBytesToRead, char *path)
 {
-    int byteOffset = bytpeOffset;
-    int numOfBytesToRead = numOofBytesToRead;
-    buffer = buffer[numOfBytesToRead];
+    unsigned char *buffer = malloc(numOfBytesToRead);
 
     int file = open(path, O_RDONLY | O_CREAT);
-    
-    if (file != -1) {
+
+    if (file != -1)
+    {
         off_t offset = lseek(file, byteOffset, SEEK_CUR);
 
-        if (offset != -1) {
+        if (offset != -1)
+        {
             size_t readFile = read(file, buffer, numOfBytesToRead);
             return buffer;
-
-        } else {
-            printf("Theres's been an error moving the file pointer.");
         }
-
-    } else {
-        printf("There's been an error opening the files");
+        else
+        {
+            buffer = "Theres's been an error moving the file pointer.";
+        }
     }
+    else
+    {
+        buffer = "There's been an error opening the files";
+    }
+    return buffer;
 }
-
 
 int main()
 {
-    char * buffer;
+    int result = 0;
     char path[] = "fat16.img";
-    char * values = searchFile(11, 2, "fat16.img", buffer);
-    printf("%s", values);
+    unsigned char *values = searchFile(11, 2, path);
+    for (int x = 0; x < 2; x++)
+    {
+        result |= (int)values[x] << (8 * x); // bit shifting
+    }
+    printf("Bytes Per Sector: %d\n", result);
+    values = searchFile(13, 1, path);
+    printf("Sectors Per Cluster: %d\n", result);
 }
